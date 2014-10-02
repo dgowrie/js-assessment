6acf25c2d1efa72718b0caee48a5be94c3c345ed
David Gowrie <david.gowrie@gmail.com>
2014-10-02 12:48:03 -0400

    passing 'objects and context' tests

--- app/objects.js
+++ app/objects.js
@@ -4,14 +4,31 @@ define(function() {
   return {
     alterContext : function(fn, obj) {
 
+		// define a function for fn so that the following will pass
+		// expect(answers.alterContext(a.sayIt, b)).to.eql('Yo, Rebecca!');
+
+		return fn.call(obj);
+
     },
 
     alterObjects : function(constructor, greeting) {
+    	
+    	constructor.prototype.greeting = greeting;
 
     },
 
     iterate : function(obj) {
 
+    	var results = [],
+    		key;
+
+    	for (key in obj) {
+    		if (obj.hasOwnProperty(key)) {
+    			results.push(key +': '+obj[key]);
+    		}
+    	}
+
+    	return results;
     }
   };
 });
