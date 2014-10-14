d7a7eaa076f187ac97177f6d3c8e62301f5f4141
David Gowrie <david.gowrie@gmail.com>
2014-10-14 12:15:39 -0400

    solving Logical Operators tests

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -4,10 +4,34 @@ define(function() {
   return {
     or : function(a, b) {
 
+    	if ( a || b ) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     },
 
     and : function(a, b) {
 
+    	if ( a && b ) {
+    		return true;
+    	} 
+    	else {
+    		return false;
+    	}
     }
+
+    // alternative - streamlined. my solution (above) has redudancies.
+    /*
+	or : function(a, b) {
+		return a || b;
+	},
+
+	and : function(a, b) {
+		return a && b;
+	}
+	*/
+
   };
 });
